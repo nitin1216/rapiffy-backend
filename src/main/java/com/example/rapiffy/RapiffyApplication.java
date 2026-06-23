f641469c1fc50827bf7678c104129b43c0c74c3d
Nitin Pandey <nitin.pandey1765@gmail.com>
2026-06-23 20:31:22 +0530

    initial commit

---
+++ src/main/java/com/example/rapiffy/RapiffyApplication.java
@@ -0,0 +1,13 @@
+package com.example.rapiffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapiffyApplication {
+	public static void main(String[] args) {
+		System.out.println("running..");
+		SpringApplication.run(RapiffyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rapiffy/RapiffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rapiffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapiffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
