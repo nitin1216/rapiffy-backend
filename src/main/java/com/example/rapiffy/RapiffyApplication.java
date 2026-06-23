d7e5810020319a8e415e546767d201a20133c6be
Nitin Pandey <nitin.pandey1765@gmail.com>
2026-06-23 21:31:51 +0530

    added table

--- src/main/java/com/example/rapiffy/RapiffyApplication.java
+++ src/main/java/com/example/rapiffy/RapiffyApplication.java
@@ -3,11 +3,12 @@ package com.example.rapiffy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* owner - Nitin Pandey
+* */
 @SpringBootApplication
 public class RapiffyApplication {
 	public static void main(String[] args) {
-		System.out.println("running..");
 		SpringApplication.run(RapiffyApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/rapiffy/model/User.java
@@ -0,0 +1,6 @@
+package com.example.rapiffy.model;
+
+//@Entity
+//@Table(name= "user")
+public class User {
+}
